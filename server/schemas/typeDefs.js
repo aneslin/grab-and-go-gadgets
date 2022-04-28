5fb4005118c5f7b43f6e06dd8b2bb4d79a98b053
Aaron Neslin <aneslin125@gmail.com>
2022-04-27 20:20:35 -0400

    added user and auth typdef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,28 @@
-const { gql } = require("apollo-server-express")
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`{
 
     type User: {
-        _id: ID
+        _id: ID!
+        username: String!
+        email:String!
+        reservedItems:[String]
+        userType: UserType
+}
 
-    }
-}`
+    enum UserType: {
+        CLIENT
+        ADMIN
+}
 
+type Auth: {
+    token: ID!
+    user: User
+}
+
+
+
+
+}`;
 
 module.exports = typeDefs;
