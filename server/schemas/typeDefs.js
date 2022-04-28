b5f89dbd332ae776582856e72c53432495d814a3
Aaron Neslin <aneslin125@gmail.com>
2022-04-27 20:41:16 -0400

    created input for user and item

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,35 +1,55 @@
 const { gql } = require("apollo-server-express");
 //dudeate is listed as a string- we will have to do parsing to do date operations
-const typeDefs = gql`{
+//all user query should first check the ME to see if the person is authorized
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    reservedItems: [Item]
+    userType: UserType
+  }
 
-    type User: {
-        _id: ID!
-        username: String!
-        email:String!
-        reservedItems:[Item]
-        userType: UserType
-}
+  enum UserType {
+    CLIENT
+    ADMIN
+  }
 
-    enum UserType: {
-        CLIENT
-        ADMIN
-}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Auth: {
-        token: ID!
-        user: User
-}
-
-type Item: {
-    _id:ID!
-    name:String!
-    image:String!
-    description:String
-    borrower:[User]
+  type Item {
+    _id: ID!
+    name: String!
+    image: String!
+    description: String
+    borrower: [User]
     dueDate: String
-}
+  }
 
+  input Item {
+    name: String!
+    image: String!
+    description: String
 
-}`;
+  }
+
+  input User{
+    username: String!
+    email: String!
+    password: String!
+    userType: UserType
+
+  }
+
+  type Query{
+      me: User
+      users: User
+      item(_id:ID!): Item
+
+  }
+`;
 
 module.exports = typeDefs;
