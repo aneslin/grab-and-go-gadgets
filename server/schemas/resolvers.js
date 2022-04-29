ea17dae6f0a00fc1b62a695be439941e55842bf8
Aaron Neslin <aneslin125@gmail.com>
2022-04-29 16:00:57 -0400

    comments and cleaned up resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,11 @@
 const { signToken } = require("../utils/auth");
 const { AuthenticationError } = require("apollo-server-express");
-const { Items, User} = require('../models')
+const { Items, User } = require("../models");
 const resolvers = {
   Query: {
     //find all users - context.user is required for security
     users: async (parent, args, context) => {
-      console.log(context.user)
+      console.log(context.user);
       if (context.user.userType === "ADMIN") {
         userData = await User.find().populate("Item", [
           "_id",
@@ -20,7 +20,7 @@ const resolvers = {
       );
       return;
     },
-
+    //get currently logged in user
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).populate(
@@ -31,63 +31,58 @@ const resolvers = {
       }
       throw new AuthenticationError("not logged in");
     },
-
+    //get one user
     user: async (parent, { username }, context) => {
-      console.log(username)
-     if (context.user.userType === "ADMIN"){
-       
-      return User.findOne({ username }).populate("Item");
-     }
-     console.log("no access")
-     return
+      console.log(username);
+      if (context.user.userType === "ADMIN") {
+        return User.findOne({ username }).populate("Item");
+      }
+      console.log("no access");
+      return;
     },
-
+    //get all items
     items: async () => {
-     const items = await Items.find()
-     return items
+      const items = await Items.find();
+      return items;
     },
-
-    item: async(parent, { _id }) => {
-        return Items.findOne({ _id})
-    }
+    //get one item by id
+    item: async (parent, { _id }) => {
+      return Items.findOne({ _id });
+    },
   },
 
   Mutation: {
+    //create a new user
     createUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
-      return {user, token};
+      return { user, token };
     },
-
+    //log in to user account
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError("invalid user"); }
-        //requires a iscorrectPassword function from models
-        const correctPw = await user.isCorrectPassword(password);
-     
-        if (!correctPw) {
-          throw new AuthenticationError("invalid user");
-        }
-        const token = signToken(user);
-        console.log(user)
-        return { user, token };
-      },
-
-      createItem: async ( parent, args, context) => {
-        if (context.user.userType==='ADMIN'){
-          console.log(args)
-          const item = await Items.create(args)
-          return {item}
-
-        }
-        throw new AuthenticationError("invalid permissions")
+        throw new AuthenticationError("invalid user");
       }
+      //requires a iscorrectPassword function from models
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("invalid user");
+      }
+      const token = signToken(user);
+      console.log(user);
+      return { user, token };
     },
-
-    
-
-  }
-;
-
-module.exports = resolvers
+    //create a new item
+    createItem: async (parent, args, context) => {
+      if (context.user.userType === "ADMIN") {
+        console.log(args);
+        const item = await Items.create(args);
+        return { item };
+      }
+      throw new AuthenticationError("invalid permissions");
+    },
+  },
+};
+module.exports = resolvers;
