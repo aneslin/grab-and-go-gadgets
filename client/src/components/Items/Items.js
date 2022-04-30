16ecb928af473eca30a684c1d5853db807e85e1f
Aaron Neslin <aneslin125@gmail.com>
2022-04-30 19:05:27 -0400

    added conditionally rendered buttons to items

--- client/src/components/Items/Items.js
+++ client/src/components/Items/Items.js
@@ -2,11 +2,24 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 
 function Items(props) {
-  const { _id, name, image, description } =
+  const { _id, name, image, description, dueDate, itemStatus, page, token } =
     props;
+  const findPage = function (page, token, itemStatus) {
+    if (page === "admin" && itemStatus === "RESERVED") {
+      return <Button>Check OUT</Button>;
+    } else if (page === "admin" && itemStatus === "CHECKED_OUT") {
+      return <Button>Check In</Button>;
+    } else if (page === "gadgets" && token && itemStatus !== "RESERVED") {
+      return <Button>Reserve</Button>;
+    } else {
+      <></>;
+    }
+  };
+  console.log(props);
+
   return (
     <div>
-      <Card key={_id}>
+      <Card key={_id} style={{ width: "24rem" }}>
         {image ? (
           <Card.Img src={image} alt={`image of ${name}`} />
         ) : (
@@ -14,6 +27,11 @@ function Items(props) {
         )}
         <Card.Title>{name}</Card.Title>
         <Card.Text>{description}</Card.Text>
+        <Card.Text>
+          <span>{itemStatus}</span>
+        </Card.Text>
+        <Card.Text>{dueDate}</Card.Text>
+        {findPage(page, token, itemStatus)}
       </Card>
     </div>
   );
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,36 +2,64 @@ import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import Auth from "../utils/auth";
 import { QUERY_ME } from "../utils/queries";
-import  Items  from "../components/Items/Items";
-import { Row, Col, Button, Card } from "react-bootstrap";
+import Items from "../components/Items/Items";
+import { Row, CardColumns, Col, Button, Card, Container } from "react-bootstrap";
 
 const Profile = () => {
-  const { loading, data } = useQuery(QUERY_ME);
+ const page = "profile";
+  const { loading, data, error, refetch } = useQuery(QUERY_ME);
+  if (loading) return <p>loading data</p>
+  if (error) return(
+    <React.Fragment>
+      <p>Oops, error! </p> 
+      <button onClick={() => refetch()}>Please try again!</button>
+    </React.Fragment>
+  )
   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+  console.log(data);
 
   return (
-    <div>
+    <Container>
       <h1>My Profile </h1>
       <section>
-      <h3>My info</h3>
-      <ul>
-        <li><span>UserName</span>: {data.me.username}</li>
-        <li><span>Email</span>: {data.me.email}</li>
-        {data.me.userType === 'ADMIN'? <li>Admin Status: You are admin</li>:''}
-      </ul>
-
+        
+        <h3>My info</h3>
+        <ul>
+          <li>
+            <span>UserName</span>: {data.me.username}
+          </li>
+          <li>
+            <span>Email</span>: {data.me.email}
+          </li>
+          {data.me.userType === "ADMIN" ? (
+            <li>Admin Status: You are admin!!</li>
+          ) : (
+            ""
+          )}
+        </ul>
       </section>
-     
-      { data ? data.me.reservedItems.map((item) => (
-        <Items
-          _id={item._id}
-          name={item.name}
-          image={item.image}
-          description={item.description}
-        ></Items>
-      )) : "loading"}
-    </div>
+      <Row>
+        {loading ? (
+          <p>"loading"</p>
+        ) : (
+          data.me.reservedItems.map((item) => {
+           let {_id, name, image, description, itemStatus, dueDate } = item
+           
+           return <Col> <Items
+              page = {page}
+              token = {token}
+              _id={_id}
+              name={name}
+              image={image}
+              description={description}
+              dueDate= {dueDate}
+              itemStatus={itemStatus}
+            ></Items></Col>
+          })
+        )}
+      </Row>
+    </Container>
   );
 };
 
