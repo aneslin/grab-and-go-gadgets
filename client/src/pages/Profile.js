33ef50bf220156a68b954446ae4766bc095166e5
Aaron Neslin <aneslin125@gmail.com>
2022-04-30 14:54:14 -0400

    added context to app.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
-import { useQuery, useMutation } from "@apollo/client";
+import { useQuery} from "@apollo/client";
 import Auth from "../utils/auth";
 import { QUERY_ME } from "../utils/queries";
 
 const Profile = () => {
   const { loading, data } = useQuery(QUERY_ME);
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
   console.log(data)
-  return <div>Profile</div>;
+  
+  return (
+  <div>
+    <div>{!token ? "not logged in" : 
+    data.me.name
+    }</div>
+
+
+  </div>
+  
+  
+  
+  
+  
+  );
 };
 
 export default Profile;
