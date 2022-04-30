7b8901a1c1a053a3e4c52544e7f66c12f4a21ff7
Aaron Neslin <aneslin125@gmail.com>
2022-04-30 15:42:34 -0400

    profile auth gets user data....sometimes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import { useQuery} from "@apollo/client";
 import Auth from "../utils/auth";
 import { QUERY_ME } from "../utils/queries";
+import {Row, Col, Button, Card} from 'react-bootstrap';
+
 
 const Profile = () => {
   const { loading, data } = useQuery(QUERY_ME);
   const token = Auth.loggedIn() ? Auth.getToken() : null;
   console.log(data)
-  
+
+ 
   return (
-  <div>
-    <div>{!token ? "not logged in" : 
-    data.me.name
-    }</div>
+    <div>
+  { !data ? "loading": <span>{data.me.username}</span>
 
-
+    }
   </div>
-  
   
   
   
