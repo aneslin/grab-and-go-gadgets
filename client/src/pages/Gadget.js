7198253045779c485bf6d07d2e8c5e3e7fb78c04
Aaron Neslin <aneslin125@gmail.com>
2022-04-30 21:36:25 -0400

    added items array to gadgets

--- client/src/pages/Gadget.js
+++ client/src/pages/Gadget.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { capitalizeFirstLetter } from '../utils/helpers';
 import Items from '../components/Items/Items'
 import { QUERY_ITEMS } from '../utils/queries'
+import { Row, Col} from 'react-bootstrap'
+import {useQuery} from '@apollo/client'
 function Gadget() {
-
+ const {loading, data, error, refetch} = useQuery(QUERY_ITEMS)  
     const [pages] = useState([
         {
             name: "Gadgets"
         }
     ]);
     const [currentPage] = useState(pages[0]);
-
+console.log(data)
     return (
 
         
@@ -25,6 +27,33 @@ function Gadget() {
                         </p>
                     </div>
                 </div>
+                <Row>
+                {loading ? (
+          <p>"loading"</p>
+        ) : (
+          data.items.map((item) => {
+            let { _id, name, image, description, itemStatus, dueDate } = item;
+
+            return (
+              <Col key = {_id}>
+                {" "}
+                <Items
+                 
+                  
+                  
+                  _id={_id}
+                  name={name}
+                  image={image}
+                  description={description}
+                  dueDate={dueDate}
+                  itemStatus={itemStatus}
+                ></Items>
+              </Col>
+            );
+          })
+        )}
+                </Row>
+
             </div>
         </div>
     )
